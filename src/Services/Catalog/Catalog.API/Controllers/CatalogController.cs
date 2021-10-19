4bc3cb3234c27641adf5a3a9fd320948d35eca49
andu <AnduamlakBerhanu@rideau.com>
2021-10-19 09:41:47 +0300

    added catalog api

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController:ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository,ILogger<CatalogController> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+        
+        // [HttpGet("{id:length(24)}",Name = "GetProduct")]
+        // public async Task<ActionResult<Product>> GetProduct
+    }
+}
--- src/Services/Catalog/Catalog.API/Controllers/WeatherForecastController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/WeatherForecastController.cs
@@ -35,5 +35,6 @@ namespace Catalog.API.Controllers
             })
             .ToArray();
         }
+
     }
 }
