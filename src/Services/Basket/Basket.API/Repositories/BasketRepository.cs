49af48751e5ca04471e97f62bdac38d4040f6825
andu <AnduamlakBerhanu@rideau.com>
2021-10-23 17:54:13 +0300

    added basket api

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository:IBasketRepositories
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+        
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var baseket = await _redisCache.GetStringAsync(userName);
+            return baseket == null ? null : JsonConvert.DeserializeObject<ShoppingCart>(baseket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
